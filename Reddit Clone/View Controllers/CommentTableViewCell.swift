1b4235c49d2b65e0bf099d24123bef15b550792d
Moin Uddin <U_Moin75@yahoo.com>
2018-10-30 20:26:01 -0400

    Setup CommentTVC and Custom Cell

---
+++ Reddit Clone/View Controllers/CommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentTableViewCell.swift
+//  Reddit Clone
+//
+//  Created by Moin Uddin on 10/30/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
